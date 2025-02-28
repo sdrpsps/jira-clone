aab64c9978fc10a3ba74e8bb8c11268f0c4c8565
sunny <sunny@bytespark.app>
2025-02-28 17:46:14 +0800

    feat: tasks api endpoint

---
+++ features/tasks/types.ts
@@ -0,0 +1,20 @@
+import { Models } from "node-appwrite";
+
+export type Task = Models.Document & {
+  name: string;
+  status: TaskStatus;
+  workspaceId: string;
+  projectId: string;
+  assigneeId: string;
+  dueDate: string;
+  description: string;
+  position: number;
+};
+
+export enum TaskStatus {
+  BACKLOG = "BACKLOG",
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  IN_REVIEW = "IN_REVIEW",
+  DONE = "DONE",
+}
