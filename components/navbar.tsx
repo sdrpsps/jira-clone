9b77a87ecaa6e965bbcc3371b51224c82ee9fdd7
sunny <sunny@bytespark.app>
2025-03-25 16:39:43 +0800

    feat: dynamic layout title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import UserButton from "@/features/auth/components/user-button";
 import MobileSidebar from "./mobile-sidebar";
+import { usePathname } from "next/navigation";
+
+const defaultPathname = {
+  title: "Home",
+  description: "Monitor all of your projects and tasks",
+};
+
+const pathnameMap = {
+  tasks: {
+    title: "My Tasks",
+    description: "View all of your tasks here",
+  },
+  projects: {
+    title: "My Projects",
+    description: "View tasks of your projects here",
+  },
+};
 
 const Nav = () => {
+  const pathname = usePathname();
+  const pathnameParts = pathname.split("/");
+  const pathnameKey = pathnameParts[3] as keyof typeof pathnameMap;
+
+  const { title, description } = pathnameMap[pathnameKey] || defaultPathname;
+
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
       <div className="flex-col hidden lg:flex">
-        <h1 className="text-2xl font-semibold">Home</h1>
-        <p className="text-muted-foreground">
-          Monitor all of your projects and tasks
-        </p>
+        <h1 className="text-2xl font-semibold">{title}</h1>
+        <p className="text-muted-foreground">{description}</p>
       </div>
       <MobileSidebar />
       <UserButton />
